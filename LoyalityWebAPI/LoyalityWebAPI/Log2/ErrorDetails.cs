d01f7b4e5608d32cf216c2ddaa41905c4729feb2
Sanjay Kumar <30061567@adani.com>
2021-11-26 15:00:20 +0530

    new logger implemented

---
+++ LoyalityWebAPI/LoyalityWebAPI/Log2/ErrorDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyalityWebAPI.Logging
+{
+    public class ErrorDetails1
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
